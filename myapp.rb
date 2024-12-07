5de8a2ea924918b4c043e0b958e1f41c0235e12a
toshimaru <me@toshimaru.net>
2024-12-07 20:43:20 +0900

    feat: Add Sentry APM
    ref. https://docs.sentry.io/platforms/ruby/guides/rack/

--- myapp.rb
+++ myapp.rb
@@ -1,4 +1,16 @@
+# frozen_string_literal: true
+
+# ref. https://docs.sentry.io/platforms/ruby/guides/rack/
+require 'sentry-ruby'
+Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
+  config.breadcrumbs_logger = [:sentry_logger, :http_logger]
+  config.traces_sample_rate = 1.0
+  config.profiles_sample_rate = 1.0
+end
+
 require 'sinatra'
+use Sentry::Rack::CaptureExceptions
 
 get '/' do
   'Hello world!'
